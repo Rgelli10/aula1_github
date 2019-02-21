3e60c000e97c9cd03fd82a50fd14f24fa4403fe3
Rgelli10 <ricardo.gelli@hotmail.com>
2019-02-21 16:21:50 -0300

    Acrescentada mensagens

--- course/src/application/Program19.java
+++ course/src/application/Program19.java
@@ -7,8 +7,9 @@ public class Program19 {
 	public static void main(String[] args) {
 
 		JOptionPane.showConfirmDialog(null, "Bom dia");
-		
 		System.out.println("Ricardo");
+		System.out.println("Boa tarde");
+		System.out.println("Boa noite");
 	}
 
 }
